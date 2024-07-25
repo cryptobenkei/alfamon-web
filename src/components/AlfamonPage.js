b8031df2c481a8f4742ec6fbfe95d62ec0df4527
Cryptobenkei <cryptobenkei@protonmail.com>
2024-07-25 07:55:11 +0200

    new version of the web

--- src/components/AlfamonPage.js
+++ src/components/AlfamonPage.js
@@ -24,15 +24,43 @@ const AlfamosPage = () => {
 
         </section>
 
+        <section className="border border-green-400 p-12">
+          <h3 className="text-sm font-semibold mb-4 pixelated text-green-200">Step One</h3>
+          <h2 className="text-xl font-semibold mb-4 pixelated">Mint an Egg</h2>
+          <p className="mt-4 text-xs text-green-200">
+            Mint an egg by casting the Alfamon frame. There are a Limited daily drop of eggs! Get yours before they run out
+          </p>
+          <a 
+              href="https://warpcast.com/~/compose?text=I want to play Alfamon https://frame.ctx.xyz/alfamon"
+              className="mt-4 bg-green-400 text-black px-4 py-2 rounded hover:bg-green-300 transition-colors w-full inline-block text-center text-xs"
+              target='_blank'
+            >
+              Mint in Farcaster
+            </a>
+        </section>
+        <section className="border border-green-400 p-12">
+          <h3 className="text-sm font-semibold mb-4 pixelated text-green-200">Step Two</h3>
+          <h2 className="text-xl font-semibold mb-4 pixelated">Hatch the Egg</h2>
+          <p className="mt-4 text-xs text-green-200">
+          Your AlfaMon egg appears as an interactive Farcaster frame. Nurture it daily! After 5 missions you can hatch the Egg. You'll get one of the 5 first original Alfamons
+          </p>
+          <ul className="list-disc list-inside space-y-2 text-xs mt-4 text-green-200">
+              <li>Cast your egg frame daily</li>
+              <li>Complete Farcaster missions</li>
+              <li>Watch your egg evolve</li>
+            </ul>
+            <p className="mt-4 text-xs text-gray-600">
+              Secret code is <span className='text-black'>alfamon</span>. But trainers be aware... The next batch of codes won't be such a cakewalk
+            </p>
+        </section>
         <section className="border border-green-400 p-4">
-          <h2 className="text-xl font-semibold mb-4 pixelated">Start Playing</h2>
-          <ol className="list-decimal list-inside space-y-2 text-xs">
-            <li><a href="https://warpcast.com/cryptobenkei" className="underline hover:text-green-200 transition-colors">Follow me in farcaster</a></li>
-            <li className='text-green-200'>Limited daily drop of eggs</li>
-            <li className='text-green-200'>Get yours before it runs away</li>
-            <li className='text-green-200'>Incubate your eggs in a frame</li>
-            <li className='text-green-200'>After five incubations, Hatch your egg</li>
-          </ol>
+          <h2 className="text-xl font-semibold mb-4 pixelated">Play!</h2>
+          <p className="mt-4 text-xs text-green-200">
+            When the egg has hatched you can cast the frame of your alfamon and play with it, there will be daily missions where you will get Alfamon candies for your alfamon and will allow you to levelUp the combat power. In the future we will add combats, special missions, gyms...
+          </p>
+          <p className="mt-4 text-xs text-green-200">
+            Airdrop? Who said airdrop?
+          </p>
         </section>
         
         <section className="border border-green-400 p-4">
